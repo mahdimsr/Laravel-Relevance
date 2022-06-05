a73f82dde269f119e32fa97339fae5a633a4a466
Mahdi <mahdi.msr4@gmail.com>
2022-06-05 20:17:42 +0430

    set user_id column from config file

--- config/relevance.php
+++ config/relevance.php
@@ -7,6 +7,7 @@ return [
      * database configuration
      */
     'database' => [
+        'relevance_user_foreign_key_column_name' => 'user_id',
         'relevance_table_name' => 'relevance_table',
         'relevance_column_name' => 'relevance',
         'relevance_relation_column_name' => 'relation_name',
--- database/migrations/create_relevance_table.php
+++ database/migrations/create_relevance_table.php
@@ -9,7 +9,7 @@ return new class () extends Migration {
     {
         Schema::create(config('relevance.database.relevance_table_name', 'relevance_table'), function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger(config('relevance.database.relevance_user_foreign_key_column_name', 'user_id'));
             $table->morphs(config('relevance.database.relevance_column_name', 'relevance'));
             $table->string(config('relevance.database.relevance_relation_column_name', 'relation_name'));
             $table->timestamps();
