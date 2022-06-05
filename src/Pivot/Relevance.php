e910d5267d41bc392a4bf32185a55cba6b94d0ae
Mahdi <mahdi.msr4@gmail.com>
2022-06-05 14:16:33 +0430

    remove properties to run actions in github

--- src/Pivot/Relevance.php
+++ src/Pivot/Relevance.php
@@ -4,13 +4,6 @@ namespace Msr\LaravelRelevance\Pivot;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-/**
- * @property int    id
- * @property int    user_id
- * @property string relevance_type
- * @property int    relevance_id
- * @property string relation_name
- */
 class Relevance extends Pivot
 {
     public $incrementing = true;
